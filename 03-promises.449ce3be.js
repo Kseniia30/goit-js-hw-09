f4a5ebe44149d78fd4bc77c396724e8b2a1a5159
Kseniia30 <100358399+Kseniia30@users.noreply.github.com>
2022-08-21 18:51:23 +0000

    Deploying to gh-pages from @ Kseniia30/goit-js-hw-09@a6acc15d415728acd242d63747c25f0117447ede 🚀

---
+++ 03-promises.449ce3be.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]"),document.querySelector("[type=submit]");
+//# sourceMappingURL=03-promises.449ce3be.js.map
---
+++ 03-promises.65fdf80e.js
@@ -0,0 +1,2 @@
+document.querySelector(".form"),document.querySelector("[name=delay]"),document.querySelector("[name=step]"),document.querySelector("[name=amount]"),document.querySelector("[type=submit]");
+//# sourceMappingURL=03-promises.65fdf80e.js.map
