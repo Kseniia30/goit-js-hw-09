8a6342cb17be87a3f02528f2c27e0a0ffd05a373
Kseniia30 <kellyshikova3004@gmail.com>
2022-08-24 10:33:48 +0200

    add task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,14 +1,51 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formField = document.querySelector(".form");
-const delay = document.querySelector("[name=delay]")
-const step = document.querySelector("[name=step]")
-const amount = document.querySelector("[name=amount]")
-const subBtn = document.querySelector("[type=submit]")
+
+let delayNew = null
+let stepNew = null
+let amountNew = null
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+}
+
+
+
+function submit(evt) {
+  evt.preventDefault();
+  if (!evt.target.tagName === "BUTTON") {
+    return
+  }
+
+    const {elements: { delay, step, amount}, } = evt.currentTarget;
+
+  delayNew = Number(delay.value)
+  stepNew = Number(step.value)
+  amountNew = Number(amount.value)
+
+  for (let i = 1; i <= amountNew; i += 1) {
+    createPromise(i, delayNew)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    
+    delayNew += stepNew
+  }
+
+  evt.currentTarget.reset();
+}
+
+formField.addEventListener("submit", submit)
